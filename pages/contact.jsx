3c4adb8462505c0a24e0b667e30c338d4f23df3e
evankski <evankarwowski@gmail.com>
2022-05-03 22:31:48 -0700

    changes form on submit

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -21,31 +21,62 @@ export default function Contact() {
               'Accept': 'application/json, text/plain, */*',
               'Content-Type': 'application/json'
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(data),
           })
+          setSubmitted(true)
+
 
       }
 
+      const renderSendButton = () => {
+        if (submitted === true) {
+          return <div><p className="message-sent">Message Sent!</p></div>;
+        } else { return (<div>
+            <div className='container'>
+              <form className='main'>
+                  <h1 className="evan">Contact me</h1>
+                <formGroup className='inputGroup'>
+                  <label htmlFor="name">Name</label>
+                  <input type="text" onChange={(e)=>{setName(e.target.value)}} name="name" className='inputField' />
+                </formGroup>
+                <formGroup className='inputGroup'>
+                  <label htmlFor="email">Email</label>
+                  <input type={email} onChange={(e)=>{setEmail(e.target.value)}} name="email" className='inputField' />
+                </formGroup>
+                <formGroup className='inputGroup'>
+                  <label htmlFor="message">Message</label>
+                  <input type="text" onChange={(e)=>{setMessage(e.target.value)}} name="message" className='inputField' />
+                </formGroup>
+                <input type="submit" onClick={(e)=>{handleSubmit(e)}} className={ submitted ? "submit" : ""}/>
+              </form>
+            </div>
+          </div>)
+        }
+    }
+
   return (
+    // <div>
+    //   <div className='container'>
+    //     <form className='main'>
+    //         <h1 className="evan">Contact me</h1>
+    //       <formGroup className='inputGroup'>
+    //         <label htmlFor="name">Name</label>
+    //         <input type="text" onChange={(e)=>{setName(e.target.value)}} name="name" className='inputField' />
+    //       </formGroup>
+    //       <formGroup className='inputGroup'>
+    //         <label htmlFor="email">Email</label>
+    //         <input type={email} onChange={(e)=>{setEmail(e.target.value)}} name="email" className='inputField' />
+    //       </formGroup>
+    //       <formGroup className='inputGroup'>
+    //         <label htmlFor="message">Message</label>
+    //         <input type="text" onChange={(e)=>{setMessage(e.target.value)}} name="message" className='inputField' />
+    //       </formGroup>
+    //       <input type="submit" onClick={(e)=>{handleSubmit(e)}} className={ submitted ? "submit" : ""}/>
+    //     </form>
+    //   </div>
+    // </div>
     <div>
-      <div className='container'>
-        <form className='main'>
-            <h1 className="evan">Contact me</h1>
-          <formGroup className='inputGroup'>
-            <label htmlFor="name">Name</label>
-            <input type="text" onChange={(e)=>{setName(e.target.value)}} name="name" className='inputField' />
-          </formGroup>
-          <formGroup className='inputGroup'>
-            <label htmlFor="email">Email</label>
-            <input type={email} onChange={(e)=>{setEmail(e.target.value)}} name="email" className='inputField' />
-          </formGroup>
-          <formGroup className='inputGroup'>
-            <label htmlFor="message">Message</label>
-            <input type="text" onChange={(e)=>{setMessage(e.target.value)}} name="message" className='inputField' />
-          </formGroup>
-          <input type="submit" onClick={(e)=>{handleSubmit(e)}} className="submit"/>
-        </form>
-      </div>
+        {renderSendButton()}
     </div>
   );
 }
